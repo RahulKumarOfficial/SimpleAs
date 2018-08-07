45f46db4a9282f8abf635a9502f863a49f4c461e
rahulkumar082 <rahulkr9622@gmail.com>
2018-08-07 19:36:56 +0530

    Added information Access area

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Fragment } from 'react';
 import Header from './Header';
 import './App.css';
 import Footer from './Footer';
+import InformationArea from './InformationArea';
 class App extends React.Component{
 
     render(){
         return(
             <div>
             <Header />
-            <div className="bottom">
-            <Footer />
-            </div>
+            <InformationArea />
             </div>
         )
     }
---
+++ src/InformationArea.js
@@ -0,0 +1,12 @@
+import React , { Component } from 'react';
+class InformationArea extends Component{
+    render(){
+        return(
+            <div>
+                text here
+            </div>
+        )
+    }
+}
+
+export default InformationArea;
