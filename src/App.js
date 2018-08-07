eab98cf4aa0ec12db72ac4da73b27a338281758e
rahulkumar082 <rahulkr9622@gmail.com>
2018-08-07 22:37:46 +0530

    Added approval area with logic

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Header from './Header';
 import './App.css';
 import Footer from './Footer';
 import InformationArea from './InformationArea';
+import ApprovalArea from './ApprovalArea.js';
+let userHOD= false;
+let userMentor = false;
+let userPrincipal = false;
 class App extends React.Component{
-
     render(){
         return(
             <div>
             <Header />
             <InformationArea />
+            {userHOD && <ApprovalArea />}
+            {userMentor && <ApprovalArea />}
+            {userPrincipal && <ApprovalArea />}
             </div>
         )
     }
